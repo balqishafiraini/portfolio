3eb938834a3e61dabc337ef5e75b5ab4a8437ce3
balqishafiraini <balqisshafiraaini@gmail.com>
2021-07-27 12:12:01 +0700

    finishing sidebar, download button, and skills dropdown

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 // Navbar
-const navLink = document.querySelectorAll('.navbar .nav-link')
+const navLink = document.querySelectorAll(".navbar .nav-link");
 navLink.forEach((e) => {
-    e.addEventListener('click', () => {
-        navLink.forEach(element => {
-            element.classList.remove('active')
-        }) 
-        e.classList.add('active')
-    })
-})
+  e.addEventListener("click", () => {
+    navLink.forEach((element) => {
+      element.classList.add("nav-hover");
+      element.classList.remove("active");
+    });
+    e.classList.add("active");
+    e.classList.remove("nav-hover");
+  });
+});
 
+// Skills
+const dropdown = document.querySelectorAll("#skills .box .dropdown");
+const content = document.querySelectorAll("#skills .box .content");
+const arrow = document.querySelectorAll("#skills .box .dropdown .arrow");
+
+// dropdown-1
+for (let i = 0; i <= dropdown.length; i++) {
+  dropdown[i].addEventListener("click", () => {
+    dropdown[i].querySelector(".arrow").classList.add("rotate");
+    content[i].querySelectorAll("span").forEach((e) => {
+      e.classList.toggle("content-disabled");
+    });
+  });
+}
